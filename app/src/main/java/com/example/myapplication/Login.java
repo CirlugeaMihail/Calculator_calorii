0547e84e5d10f17ab2e55d7d58a22b741cd105a4
Andrei <andreitopor.2002@yahoo.com>
2023-07-01 15:25:35 +0300

    Am facut modificari la design

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -3,17 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
     private Button buttonBack;
+    private EditText editText1;
+    private EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        editText1 = findViewById(R.id.textUsername);
+        editText1.setTextColor(Color.BLACK);
+        editText2 = findViewById(R.id.textPassword);
+        editText2.setTextColor(Color.BLACK);
 
         buttonBack = (Button) findViewById(R.id.btnBack);
         buttonBack.setOnClickListener(new View.OnClickListener()
--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -3,16 +3,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
  private Button buttonBack;
+    private EditText editText1;
+    private EditText editText2;
+    private EditText editText3;
+    private EditText editText4;
+    private EditText editText5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editText1 = findViewById(R.id.Register_Mail);
+        editText1.setTextColor(Color.BLACK);
+        editText2 = findViewById(R.id.Register_password1);
+        editText2.setTextColor(Color.BLACK);
+        editText3 = findViewById(R.id.Register_password2);
+        editText3.setTextColor(Color.BLACK);
+        editText4 = findViewById(R.id.textInaltime);
+        editText4.setTextColor(Color.BLACK);
+        editText5 = findViewById(R.id.textGreutate);
+        editText5.setTextColor(Color.BLACK);
 
 
         buttonBack = (Button) findViewById(R.id.btnBack);
